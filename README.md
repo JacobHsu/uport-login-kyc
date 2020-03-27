a5618f30971a9c0633169cb94a59257b9f79bde2
jacobhsu <jacob.hsu.tw@gmail.com>
2020-03-27 14:55:23 +0800

    disclosure-request-login-service

--- README.md
+++ README.md
@@ -1,6 +1,43 @@
 # uport-login-kyc
 
 `$ yarn add express body-parser ngrok`
+`$ yarn add did-jwt uport-credentials uport-transports`
+
+[disclosure-request-login-service](https://developer.uport.me/credentials/login#disclosure-request-login-service)
+
+![Alt server-login](https://developer.uport.me/static/ac1d5b0471fb8a825c4eb9f6b81db294/7a2d1/server-login.png)
+
+## sample
+
+/uportlandia/stage/issuers
+
+```json
+{
+"CITY_ID":{"did":"did:ethr:0x8bb183661b5a34de9b9335121cd6d52dda36891d","key":"e7c670843c016850d4ae3f20276e73755a6c91a2f6046abcef55f0fe42a3533e","vc":["/ipfs/QmXLhSxiQt89kY9mnWUPAZYna1a51jmqLVPf6iN4TGdJK1"]
+}
+```
+
+https://uportlandia.uport.space/city/ipfs/QmXLhSxiQt89kY9mnWUPAZYna1a51jmqLVPf6iN4TGdJK1
+
+## fix yarn setup
+
+>  Error: EthrDIDResolver requires a provider configuration for at least one network
+
+node_scripts\create_issuers.js
+
+```js
+// https://github.com/uport-project/uport-credentials#uport-credentials-library
+const RPC_URL = "https://mainnet.infura.io/v3/5ffc47f65c4042ce847ef66a3fa70d4c";
+
+const credentials = new Credentials({
+    appName: 'Gorilla Login Example',
+      did: 'did:ethr:0x136dd005fa469e33581b9623ad82b2c8f42bc9d2',
+      privateKey: 'f161a002a08b6fc9c6f54b6a0dc9b4622e32fa5a4821a440f0e7761f5880efd0',
+      ethrConfig: {
+        rpcUrl: RPC_URL //<--- Required Upgrade to uport-credentials@1.3.0
+      }
+})
+```
 
 ## References
 
--- index.js
+++ index.js
@@ -1,14 +1,73 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const ngrok = require('ngrok')
+const decodeJWT = require('did-jwt').decodeJWT
+const { Credentials } = require('uport-credentials')
+const transports = require('uport-transports').transport
+const message = require('uport-transports').message.util
 
 let endpoint = ''
 const app = express();
 app.use(bodyParser.json({ type: '*/*' }))
 
-app.get('/', function(req, res) {
-    res.send('hello world');
-});
+const RPC_URL = "https://mainnet.infura.io/v3/5ffc47f65c4042ce847ef66a3fa70d4c";
+//setup Credentials object with newly created application identity.
+const credentials = new Credentials({
+    appName: 'Gorilla Login Example',
+      did: 'did:ethr:0x136dd005fa469e33581b9623ad82b2c8f42bc9d2',
+      privateKey: 'f161a002a08b6fc9c6f54b6a0dc9b4622e32fa5a4821a440f0e7761f5880efd0',
+      ethrConfig: {
+        rpcUrl: RPC_URL //<--- Required Upgrade to uport-credentials@1.3.0
+      }
+})
+
+app.get('/', (req, res) => {
+    //Create a new disclosure request, requesting the push notification token and a new key
+    credentials.createDisclosureRequest({
+      notifications: true,
+      accountType: 'keypair',
+      vc: ['/ipfs/QmWE2pDhzcaa6jN1YQCgisBtBqF5uUCeQrfVAvGqoX4BEx'],
+      callbackUrl: endpoint + '/callback'
+    }).then(disclosureRequestJWT => {
+      console.log('get:',decodeJWT(disclosureRequestJWT))  //log request token to console
+      
+      //Create QR code with the disclosure request.
+      const uri = message.paramsToQueryString(message.messageToURI(disclosureRequestJWT), {callback_type: 'post'})
+      const qr =  transports.ui.getImageDataURI(uri)
+      res.send(`<div><img src="${qr}"/></div>`)
+    })
+})
+
+// after scan QR
+app.post('/callback', (req, res) => {
+    const access_token = req.body.access_token
+    credentials.authenticateDisclosureResponse(access_token).then(userProfile => {
+      console.log('callback:',{userProfile}, "\n\n")
+  
+      const attestation = {
+        sub: userProfile.did,
+        claim: { 
+          gorillaKYC:{
+            name: 'Test User', 
+            dni: '1234567-8',
+            kyc: 'passed'
+          }
+        }
+      }
+  
+      credentials.createVerification(attestation)
+      .then( credential => {
+        //Push credential to user
+        const pushTransport = transports.push.send(userProfile.pushToken, userProfile.boxPub)
+        return pushTransport(credential)
+      })
+      .then(pushData => {
+        console.log("Pushed to user: "+JSON.stringify(pushData))
+        
+      })
+  
+    })
+})
 
 // run the app server and tunneling service
 app.listen(8888, () => {
